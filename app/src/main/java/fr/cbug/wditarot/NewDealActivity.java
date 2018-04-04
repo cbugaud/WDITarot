9d6e2690a3f212b0448a862f92daffd74593ccf0
Celine_Bugaud <c.bugaud+git@gmail.com>
2018-04-04 14:12:29 +0200

    [CBU] NewDeal : Génération des boutons pour le preneur et le contrat

--- app/src/main/java/fr/cbug/wditarot/NewDealActivity.java
+++ app/src/main/java/fr/cbug/wditarot/NewDealActivity.java
@@ -1,26 +1,97 @@
 package fr.cbug.wditarot;
 
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.ContextThemeWrapper;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.ToggleButton;
 
 import com.google.common.collect.Lists;
 
 import java.util.List;
 
+import fr.cbug.wditarot.model.Bid;
+import fr.cbug.wditarot.model.PlayedDeal;
 import fr.cbug.wditarot.model.Player;
 
 public class NewDealActivity extends AppCompatActivity {
+    private static final String TAG = "newDealActivity";
     private List<Player> players = Lists.newArrayList(new Player("Erwan"), new Player("Céline"), new Player("Julien"), new Player("Simon"), new Player("Aude"));
-
+    private PlayedDeal deal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_deal);
 
-//        generateTakerButtons();
-//        generateBidButtons();
+        deal = new PlayedDeal();
+        generateTakerButtons();
+        generateBidButtons();
 //        generateCalleeButtons();
 //        generateBonusesButtons();
+    }
+
+    private void generateTakerButtons() {
+        Log.d(TAG, "Create taker buttons");
+        RadioGroup takerButtonsLayout = findViewById(R.id.deal_taker_layout);
+
+        for (Player player : players) {
+            RadioButton playerButton = createRadioButton(player.getName(), (view, selected) -> selectTaker(view, selected, player));
+            addRadioToGroup(takerButtonsLayout, playerButton);
+        }
+    }
+
+    private void generateBidButtons() {
+        Log.d(TAG, "Create bids buttons");
+        RadioGroup bidButtonsLayout = findViewById(R.id.deal_bids_layout);
+
+        for (Bid bid : Bid.values()) {
+            RadioButton button = createRadioButton(bid.getLabel(), (view, selected) -> selectBid(view, selected, bid));
+            addRadioToGroup(bidButtonsLayout, button);
+        }
+    }
+
+    private void selectBid(View view, boolean selected, Bid bid) {
+        if (selected) {
+            deal.setBid(bid);
+            Log.d(TAG, "Bid is "+bid);
+        }
+    }
+
+    private void selectTaker(View view, boolean selected, Player player) {
+        if (selected) {
+            deal.setTaker(player);
+            Log.d(TAG, "Taker is "+player.getName());
+        }
+    }
+
+    private RadioButton createRadioButton(String text, CompoundButton.OnCheckedChangeListener listener) {
+        RadioButton button = createRadioButton(listener);
+        button.setText(text);
+        return button;
+    }
+    private RadioButton createRadioButton(int textResId, CompoundButton.OnCheckedChangeListener listener) {
+        RadioButton button = createRadioButton(listener);
+        button.setText(textResId);
+        return button;
+    }
+    private RadioButton createRadioButton(CompoundButton.OnCheckedChangeListener listener) {
+        RadioButton button = new RadioButton(this);
+        button.setOnCheckedChangeListener(listener);
+        button.setBackgroundResource(R.drawable.my_radio_button_background);
+        button.setButtonDrawable(null);
+        return button;
+    }
+
+    private void addRadioToGroup(RadioGroup group, RadioButton button) {
+        RadioGroup.LayoutParams layoutParams = new RadioGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT,1);
+        group.addView(button, layoutParams);
     }
 }
--- app/src/main/java/fr/cbug/wditarot/model/Bid.java
+++ app/src/main/java/fr/cbug/wditarot/model/Bid.java
@@ -1,18 +1,29 @@
 package fr.cbug.wditarot.model;
 
+import fr.cbug.wditarot.R;
+
 /**
  * Les différents types de contrat (prise, garde, ...)
  */
 public enum Bid {
-    TAKE(1), GUARD(2), GUARD_WITHOUT(4), GUARD_AGAINST(6);
+    TAKE(1, R.string.bid_take),
+    GUARD(2, R.string.bid_guard),
+    GUARD_WITHOUT(4, R.string.bid_guard_without),
+    GUARD_AGAINST(6, R.string.bid_guard_against);
 
+    private int label;
     private int stakeMultiplier;
 
-    Bid(int multiplier) {
+    Bid(int multiplier, int label) {
       this.stakeMultiplier = multiplier;
+      this.label = label;
     }
 
     public int getStakeMultiplier() {
         return stakeMultiplier;
+    }
+
+    public int getLabel() {
+        return label;
     }
 }
